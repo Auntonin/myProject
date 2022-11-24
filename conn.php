a8dcbc04e946cd24d386358272fe46c0eba5e784
autnonin <autonin@gmail.com>
2022-11-24 02:07:15 -0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "food_panqueen_db";
+$dbname = "food";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
